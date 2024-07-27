f7bd3d63cfc2455dd299ef989a1c5c22b021be47
Daniel RICKLIN <ricklin.daniel@outlook.fr>
2024-07-27 16:42:00 +0200

    first commit

---
+++ srv_demo.tf
@@ -0,0 +1,58 @@
+variable "ssh_key" {
+  type      = string
+  sensitive = true
+}
+
+resource "proxmox_vm_qemu" "srv_demo" {
+  # count = 2
+  # name  = "srv-demo-${count.index + 1}"
+  name        = "srv-demo-1"
+  target_node = "pve"
+  clone       = "ubuntu-noble-cloud-template2"
+  full_clone  = true
+
+  agent = 1
+
+  cores   = 2
+  sockets = 1
+  cpu     = "host"
+  memory  = 4096
+
+  os_type = "cloud-init"
+  scsihw  = "virtio-scsi-pci"
+
+  network {
+    bridge = "vmbr0"
+    model  = "virtio"
+    tag    = "150"
+  }
+
+  disks {
+    ide {
+      ide3 {
+        cloudinit {
+          storage = "DATA"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          storage = "DATA"
+          size    = "20G"
+        }
+      }
+    }
+  }
+
+  boot = "order=scsi0"
+
+  #   ipconfig0  = "ip=10.1.192.${26 + count.index + 1}/29,gw=10.1.192.25"
+  ipconfig0 = "ip=dhcp"
+  # nameserver = "9.9.9.9"
+
+  sshkeys = <<EOF
+        ${var.ssh_key}
+        EOF
+
+}
